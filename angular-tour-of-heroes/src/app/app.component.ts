416ef64f6b091f9dc46df9cf17c0529a6057dd6e
Andrea Verdura <andrea.verdura3@gmail.com>
2021-10-18 16:35:00 +0200

    Finished chapter5. Tutorial ended

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MessagesComponent } from './messages/messages.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
